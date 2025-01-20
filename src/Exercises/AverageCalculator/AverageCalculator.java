6472f4eb4c96a451574995e8b02ae8600285bfbd
max-monty <maxmonty95@gmail.com>
2025-01-19 19:43:48 -0500

    first commit

---
+++ src/Exercises/AverageCalculator/AverageCalculator.java
@@ -0,0 +1,17 @@
+package Exercises.AverageCalculator;
+import java.util.ArrayList;
+
+public class AverageCalculator
+{
+    public static void main(String[] args) {
+        
+    }
+    
+    public static double getAverageOf (ArrayList<Integer> numbers)
+    {
+        /* TODO */
+        return 0.0;
+    }
+
+    
+}
